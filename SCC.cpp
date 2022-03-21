090d0738992bcfbf889e3ac565170f8593b172d6
dogdogeatcatcat <r09946017@ntu.edu.tw>
2022-03-21 17:35:23 +0800

    release reserved memory

--- SCC.cpp
+++ SCC.cpp
@@ -10,7 +10,7 @@ typedef struct Graph {
 
 class Condensation {
 private:
-	graph* G = new graph();		// The original graph
+	graph* org_G = new graph();	// The original graph
 	graph* rev_G = new graph();	// The transpose graph	
 	
 	vector<bool> visited;		// Record the vertices which are already visited in DFS 
@@ -18,40 +18,47 @@ private:
 	vector<int> component;		// Store the current strongly connected component
 public:
 	Condensation () {
-		G->offset.push_back(0);	// Add one more offset that makes the code simpler
+		org_G->offset.push_back(0);	// Add one more offset that makes the code simpler
 	}
 
 	~Condensation () {
+		delete org_G;
+		delete rev_G;
 	}
 	
 	void add_node () {
 
-		G->offset.push_back(G->data.size());	// Points to the end of data
+		org_G->offset.push_back(org_G->data.size());	// Points to the end of data
 
 		return;
 	}
 
 	void add_edge (int from, int to) {
 
-		G->data.insert(G->data.begin() + G->offset[from], to);
+		org_G->data.insert(org_G->data.begin() + org_G->offset[from], to);
 
 		// Update the offsets
-		for (int i = from + 1; i < G->offset.size(); i ++)
-			G->offset[i] ++;
+		for (int i = from + 1; i < org_G->offset.size(); i ++)
+			org_G->offset[i] ++;
 
 		return;			
+	}
+
+	graph* get_graph () {
+		
+		return org_G; 	
 	}
 
 	void log_graph () {
 
 		cout << "===== Original Graph =====" << endl;
 
-		for (int i = 0; i < G->offset.size() - 1; i ++) {
+		for (int i = 0; i < org_G->offset.size() - 1; i ++) {
 			cout << i << " :";
 			
 			// Print its neighbors of vertex i
-			for (int j = G->offset[i]; j < G->offset[i+1]; j ++)
-				cout << " " << G->data[j];
+			for (int j = org_G->offset[i]; j < org_G->offset[i+1]; j ++)
+				cout << " " << org_G->data[j];
 			cout << endl;
 		}
 
@@ -64,14 +71,14 @@ public:
 		rev_G->offset.clear();
 		rev_G->data.clear();
 
-		for (int i = 0; i < G->offset.size(); i ++)
+		for (int i = 0; i < org_G->offset.size(); i ++)
 			rev_G->offset.push_back(0);
 
-		for (int i = 0; i < G->offset.size() - 1; i ++) {
-			for (int j = G->offset[i]; j < G->offset[i + 1]; j ++) {
+		for (int i = 0; i < org_G->offset.size() - 1; i ++) {
+			for (int j = org_G->offset[i]; j < org_G->offset[i + 1]; j ++) {
 				
 				// Reverse the direction of the edge
-				int from = G->data[j], to = i;	
+				int from = org_G->data[j], to = i;	
 				rev_G->data.insert(rev_G->data.begin() + rev_G->offset[from], to);
 
 				// Update the offsets
@@ -103,9 +110,9 @@ public:
 
 		visited[v] = true;
 
-		for (int i = G->offset[v]; i < G->offset[v + 1]; i ++)
-			if (!visited[G->data[i]])
-				dfs(G->data[i]);
+		for (int i = org_G->offset[v]; i < org_G->offset[v + 1]; i ++)
+			if (!visited[org_G->data[i]])
+				dfs(org_G->data[i]);
 
 		exit_time.push_back(v);
 		
@@ -124,9 +131,11 @@ public:
 		return;
 	}
 
-	vector<int> condensation() {
+	vector<int> get_condensation_mapping() {
 
-		int num_of_node = G->offset.size() - 1;
+		graph_transpose();
+
+		int num_of_node = org_G->offset.size() - 1;
 		visited.assign(num_of_node, false);
 
 		for (int i = 0; i < num_of_node; i ++)
@@ -141,8 +150,8 @@ public:
 
 		for (int i = 0; i < num_of_node ;i ++)
 			if (!visited[exit_time[i]]) {
-				cur_mapping ++;	// Next strongly connected component
-				rev_dfs(exit_time[i]);
+				cur_mapping ++;		// Next strongly connected component
+				rev_dfs(exit_time[i]);	// Perform DFS on transpose graph to get the SCC of the current vertex
 				for (int j = 0; j < component.size(); j ++)
 					mapping[component[j]] = cur_mapping;
 				component.clear();
@@ -152,6 +161,47 @@ public:
 	}
 };
 
+void Test (int num_of_vertices, vector<vector<int>>& edges, int debug = 1) {
+	
+	Condensation* tmp = new Condensation();
+
+	// Construct the graph
+	for (int i = 0; i < num_of_vertices; i ++)
+		tmp->add_node();
+	for (auto& e : edges)
+		tmp->add_edge(e[0], e[1]);
+
+	// Print the graphs, just for debugging
+	if (debug) {
+		tmp->log_graph();
+		tmp->graph_transpose();
+		tmp->log_graph_transpose();
+	}
+	
+		
+	// Output final graph structure
+	graph* G = tmp->get_graph();
+	cout << "===== Graph Structure ====" << endl;
+	cout << "Offset :";
+	for (auto& offset : G->offset)
+		cout << " " << offset;
+	cout << endl;
+	cout << "Data :";
+	for (auto& data : G->data)
+		cout << " " << data;
+	cout << endl;
+
+	// Get the SCC id for each vertex
+	vector<int> mapping = tmp->get_condensation_mapping();
+	cout << "===== Mapping =====" << endl;
+	for (int i = 0; i < num_of_vertices; i ++)
+		cout << mapping[i] << ' ';
+	cout << endl;
+
+	delete tmp;
+
+	return;
+}
 
 int main() {
 
@@ -164,35 +214,50 @@ int main() {
 		./a.out	
 	*/
 
-	int num_of_vertices = 8;
-	vector<vector<int>> edges = {
-		{1, 2}, {1, 3}, {1, 4},
-		{2, 3}, {2, 4},
-		{3, 4},
-		{4, 5}, {4, 6},
-		{6, 7},
-		{7, 4}
+	int debug = 1;
+
+	// Example 1 from Wikipedia
+
+	int Ex1_num_of_vertices = 8;
+	vector<vector<int>> Ex1_edges = {
+		{0, 1},
+		{1, 2}, {1, 4}, {1, 5},
+		{2, 3}, {2, 6},
+		{3, 2}, {3, 7},
+		{4, 0}, {4, 5},
+		{5, 6},
+		{6, 5},
+		{7, 3}, {7, 6}
 	};
 
-	Condensation* tmp = new Condensation();
+	Test (Ex1_num_of_vertices, Ex1_edges);
 
-	// Construct the graph
-	for (int i = 0; i < num_of_vertices; i ++)
-		tmp->add_node();
-	for (auto& e : edges)
-		tmp->add_edge(e[0], e[1]);
+	// Example 2 from Wikipedia
 
-	// Print the graphs, just for debugging
-	tmp->log_graph();
-	tmp->graph_transpose();
-	tmp->log_graph_transpose();
+	int Ex2_num_of_vertices = 16;
+	vector<vector<int>> Ex2_edges = {
+		{0, 2},
+		{1, 0}, {1, 5},
+		{2, 1}, {2, 4},
+		{3, 2}, {3, 9},
+		{4, 3}, {4, 5}, {4, 10},
+	
+		{5, 6}, {5, 8}, {5, 13},
 
-	// Get the SCC id for each vertex
-	vector<int> mapping = tmp->condensation();
-	cout << "===== Mapping =====" << endl;
-	for (int i = 0; i < num_of_vertices; i ++)
-		cout << mapping[i] << ' ';
-	cout << endl;
+		{6, 7},
+		{7, 8}, {7, 15},
+		{8, 6}, {8, 15},
+
+		{9, 10},
+		{10, 11}, {10, 12},
+		{11, 9},
+		{12, 11}, {12, 13},
+
+		{13, 15}, {13, 14},
+		{14, 13},
+	};
+
+	Test (Ex2_num_of_vertices, Ex2_edges);
 
 	return 0;
 }
